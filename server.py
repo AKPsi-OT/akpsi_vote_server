653cd98c3c39ddab9127bbd3c90640ed16a5ed54
cnexus <cnexus0@gmail.com>
2016-10-04 21:41:51 -0400

    server: add implementation for submitting a vote

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from gevent import monkey
 monkey.patch_all()
 
 import time
+from collections import defaultdict
 from threading import Thread
 from flask import Flask, render_template, session, request
 from flask.ext.socketio import SocketIO, emit, disconnect
@@ -23,6 +24,9 @@ admins = set()
 admins.add('cgonza1')
 
 clients = set()
+votes = defaultdict(lambda: defaultdict(int)); # 2D defaultdict, where votes[choice][rush_name] = count
+current_name = ""
+
 thread = None
 
 def background_thread():
@@ -53,6 +57,7 @@ def index():
         return render_template('error.html', error="duplicate")
     else:
         clients.add(cas.username)
+        print('Clients is: ' + clients)
         return render_template('index.html', username = cas.username)
 
 @app.route('/admin')
@@ -70,6 +75,7 @@ def admin_panel():
 @socketio.on('start_vote', namespace='/vote')
 def start_vote(msg):
     if cas.username in admins:
+        current_name = msg['name']
         emit('vote_start', {'name': msg['name'], 'abstain': msg['abstain']}, broadcast=True)
 
 @socketio.on('end_vote', namespace='/vote')
@@ -89,8 +95,17 @@ def test_message(message):
          {'data': message['data'], 'count': session['receive_count']})
 
 @socketio.on('submit_vote', namespace='/vote')
-def function():
-    pass
+def function(vote):
+    votes[vote['bid']][current_name] += 1
+    votes_cast = 0
+    votes_left = 0
+
+    for key in votes:
+        votes_cast += votes[key][current_name]
+
+    votes_left = len(clients) - votes_cast
+
+    emit('vote_submitted', {'name':current_name, 'votes_cast': votes_cast, 'votes_left': votes_left})
 
 @socketio.on('disconnect_req', namespace='/vote')
 def disconnect_request():
@@ -105,7 +120,7 @@ def socket_attach():
 
 @socketio.on('disconnect', namespace='/vote')
 def socket_detach():
-    print('Socket disconnected')
+    print('Socket disconnected from user: ' + cas.username)
 
 if __name__ == "__main__":
     # Fetch the environment variable (so it works on Heroku):
