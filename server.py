e402bceba563cd0865184ea8cbe3656e7df85383
rchetal <raghav.chetal@gmail.com>
2018-12-16 20:57:18 -0500

    updated permissions

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ ADMINS.add('cgonza1')
 ADMINS.add('rchetal')
 ADMINS.add('acerc')
 ADMINS.add('kcotter2')
-ADMINS.add('nmodi')
+ADMINS.add('alice')
 # Add VP membership, VP finance, and President IDs to ADMINS
 
 has_voted = set()
