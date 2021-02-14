471db6b2b4d9f85e4b4323b8c238a1d7c8fa13de
AdvaitIshwar <45025807+AdvaitIshwar@users.noreply.github.com>
2021-02-13 19:57:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -261,4 +261,4 @@ def socket_detach():
 
 if __name__ == "__main__":
     # Fetch the environment variable (so it works on Heroku):
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 21697)))
