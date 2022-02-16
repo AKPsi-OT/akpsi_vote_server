3080756ed0f1ab3639a590d7c206e2c163ad5ec1
Amod Mathur <45135640+amodmathur@users.noreply.github.com>
2022-02-15 22:52:28 -0500

    Update server.py

--- server.py
+++ server.py
@@ -26,8 +26,8 @@ BID_THRESHOLD = 0.75
 
 ADMINS = set()
 ADMINS.add('cgonza1')
-ADMINS.add('aleipold')
-ADMINS.add('cfeeley1')
+ADMINS.add('amodm104')
+ADMINS.add('rtiwari1')
 ADMINS.add('jhan0810')
 ADMINS.add('vpatil1')
 ADMINS.add('hmutyala')
